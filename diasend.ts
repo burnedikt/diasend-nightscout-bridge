6f2dcadcd2ce52a6de84fa7c600c70a94e595f57
Benedikt Reiser <benedikt.reiser@gmail.com>
2022-08-27 19:39:39 +0200

    feat: scrape basal rate from diasend website

--- diasend.ts
+++ diasend.ts
@@ -1,9 +1,13 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 import config from "./config";
 import { stringify } from "querystring";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import NodeCache from "node-cache";
+import { wrapper } from "axios-cookiejar-support";
+import { CookieJar } from "tough-cookie";
+import randUserAgent from "rand-user-agent";
+import { load } from "cheerio";
 
 type GlucoseUnit = "mg/dl" | "mmol/l";
 
@@ -122,4 +126,85 @@ export async function getPatientData(
   );
 
   return response.data;
+}
+
+// SCRAPER for website
+const diasendWebsiteBaseUrl = "https://international.diasend.com/";
+
+export async function getAuthenticatedScrapingClient({
+  username,
+  password,
+  country = 108,
+  locale = "en_US",
+}: {
+  username: string;
+  password: string;
+  country?: number;
+  locale?: string;
+}) {
+  const client = wrapper(
+    axios.create({
+      baseURL: diasendWebsiteBaseUrl,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+      jar: new CookieJar(),
+      // use a random user agent to scracpe
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      headers: { "User-Agent": randUserAgent() as string },
+    })
+  );
+  try {
+    await client.post(
+      "/diasend/includes/account/login.php",
+      stringify({ country, locale, user: username, passwd: password }),
+      {
+        // withCredentials: true,
+        // we don't want the actual redirect to the dashboard to happen (as it wouldn't have cookies set)
+        maxRedirects: 0,
+      }
+    );
+    throw new Error('Login request cannot be "successful"');
+  } catch (err) {
+    const redirectResponse = (err as AxiosError).response;
+    const userId = redirectResponse?.headers["location"].match(
+      /\/reports\/(?<userId>.*)\/summary/
+    )?.groups?.userId;
+    if (!userId) {
+      throw new Error("Could not find userId to scrape diasend");
+    }
+
+    // remember the PHPSESSID (to authenticate future requests) --> done automatically by the cookiejar
+    // and the "userId" which can be obtained from the redirect happening after login and is required to access reports etc.
+    return { client, userId };
+  }
+}
+
+export async function getPumpSettings(client: AxiosInstance, userId: string) {
+  const { data } = await client.get<string>(
+    `/reports/${userId}/insulin/pump-settings`
+  );
+  const $ = load(data);
+  // find the active basal profile
+  const activeBasalProfile = $("td")
+    .filter((_, ele) => $(ele).text() === "Active basal program")
+    .next()
+    .text();
+  const basalProfile: [string, number][] = (
+    $("h4")
+      .filter((_, e) =>
+        $(e).text().startsWith(`Program: ${activeBasalProfile}`)
+      )
+      .next("table")
+      // get all rows except for header row
+      .find("tr:not(:first)")
+      .map((_, row) =>
+        $(row)
+          // get all cells of a row
+          .children("td")
+          // take only the last two cells (start time and rate)
+          .slice(-2)
+          .map((_, cell) => $(cell).text())
+      )
+      .get() as [string, string][]
+  ).map(([startTime, rate]) => [startTime, parseFloat(rate)]);
+  return basalProfile;
 }
